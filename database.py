c6f2482982fbc24ce270faa431c4b5d2ea5e3e95
Nathan Freeman <nathanlfreeman@gmail.com>
2023-06-21 18:39:58 -0400

    had to recreate database connection string; then added line for ssl certificate

--- database.py
+++ database.py
@@ -1,4 +1,17 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 
 # engine creation: format of connection string-- "mysql+pymysql://<username>:<password>@<hostname>/<db_name>?charset=utf8mb4"
-engine = create_engine("mysql+pymysql://x414bwf9vek8uggv0kbx:pscale_pw_3BQq9kkcdTh0o78rIp8KnHAajpH438XlLfJVK3aGdWT@aws.connect.psdb.cloud/bugnaught?charset=utf8mb4")
+
+db_conn_string = "mysql+pymysql://iamzt7rxtuqn4l1qm23v:pscale_pw_DI2bxknV2sS5y08jdOQY6MAUsXYbNXd5C0BsgbKzMhR@aws.connect.psdb.cloud/bugnaught?charset=utf8mb4"
+engine = create_engine(
+    db_conn_string, 
+    connect_args={
+        "ssl": {
+            "ca": "/etc/ssl/cert.pem"
+        }
+    }
+)
+
+with engine.connect() as conn:
+    result = conn.execute(text("SELECT * FROM users"))
+    print(result.all())
