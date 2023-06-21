2b1929268c93653bc3ec436ce7e1f8fa56953390
Nathan Freeman <nathanlfreeman@gmail.com>
2023-06-21 12:50:24 -0400

    add first api endpoint; change port to avoid conflict

--- app.py
+++ app.py
@@ -1,10 +1,41 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+USERS = [
+    {
+        "_id": 1,
+        "first_name": "Nathan",
+        "last_name": "Freeman",
+        "roles": ["admin","developer","manager","lead"]
+    },
+    {
+        "_id": 2,
+        "first_name": "Sujan",
+        "last_name": "Trivedi",
+        "roles": ["developer","manager","lead"]
+    },
+    {
+        "_id": 3,
+        "first_name": "Jeff",
+        "last_name": "Kitrosser",
+        "roles": ["developer","manager"]
+    },
+    {
+        "_id": 4,
+        "first_name": "Olivia",
+        "last_name": "Wills",
+        "roles": ["developer"]
+    },
+]
+
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Hello, Nate!</p>"
+
+@app.route("/api/users")
+def get_users():
+    return jsonify(USERS)
 
 # a good way to run program via python command rather than flask run command
 if(__name__ == "__main__"):
